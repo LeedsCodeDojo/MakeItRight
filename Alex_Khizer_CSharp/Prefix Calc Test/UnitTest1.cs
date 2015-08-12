555c7ef959ab5b91c3421ab231e81f10cb45a95c
Grant Crofton <grantac@hotmail.com>
2015-08-12 08:23:46 +0100

    Added Alex & Khizer cange

---
+++ Alex_Khizer_CSharp/Prefix Calc Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prefix_Calc;
+
+namespace Prefix_Calc_Test {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void OnePlusOneShouldReturnTwo() {
+            var result = PrefixCalculator.Evaluate("+ 1 1");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void OnePlusTwoShouldReturnThree() {
+            var result = PrefixCalculator.Evaluate("+ 1 2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TwoPlusTwoShouldReturnFour() {
+            var result = PrefixCalculator.Evaluate("+ 2 2");
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TwoMinusOneShouldReturnOne() {
+            var result = PrefixCalculator.Evaluate("- 2 1");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TenMultiplyTwoShouldReturnTwenty() {
+            var result = PrefixCalculator.Evaluate("* 10 2");
+            Assert.AreEqual(20, result);
+        }
+
+        [TestMethod]
+        public void TenDividedByTwoShouldReturnFive() {
+            var result = PrefixCalculator.Evaluate("/ 10 2");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void NineDividedByTwoShouldReturnFour() {
+            var result = PrefixCalculator.Evaluate("/ 9 2");
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void AddFromOneToNineShouldReturnFortyFive() {
+            var result = PrefixCalculator.Evaluate("+ 1 2 3 4 5 6 7 8 9");
+            Assert.AreEqual(45, result);
+        }
+    }
+}
---
+++ Alex_Khizer_CSharp/Prefix Calc/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prefix_Calc
+{
+ 
+    public class PrefixCalculator
+    {
+
+        public static Int32 Evaluate(String Input)
+        {
+            var parts = Input.Split(' ');
+            var operation = parts[0];
+
+            var result = Int32.Parse(parts[1]);
+
+            for (int i = 2; i < parts.Length; i++) {
+                var nextPart = Int32.Parse(parts[i]);
+                switch (operation) {
+                    case "+":
+                        result = result + nextPart;
+                        break;
+                    case "-":
+                        result = result - nextPart;
+                        break;
+                    case "*":
+                        result = result * nextPart;
+                        break;
+                    case "/":
+                        result = result / nextPart;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return result;
+
+        }
+    }
+
+}
